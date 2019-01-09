fc7359863b1601d386bf0bff030c94a1ab038563
RadikMih <radik.mihnev@gmail.com>
2019-01-09 19:13:20 +0200

    Adding employee service

--- src/main/java/jdbcdemo/web/EmployeeService.java
+++ src/main/java/jdbcdemo/web/EmployeeService.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -19,19 +21,24 @@ public class EmployeeService {
         String configFile = "src\\main\\resources\\application.properties";
         Properties dbConfig = new Properties();
         try (FileInputStream fis = new FileInputStream(configFile)){
-            
+            dbConfig.load(fis);
+        } catch (FileNotFoundException e) {
+            System.out.println("Properties file not found");
+        } catch (IOException e){
+            System.out.println("Unable to read properties file");
         }
 
 
 
-        String dbUrl = "jdbc:mysql://localhost:3306/telerikacademy";
-        String username = "dev_user";
-        String password = "dev_user";
+        String dbUrl = dbConfig.getProperty("dbUrl");
+        String username = dbConfig.getProperty("username");
+        String password = dbConfig.getProperty("password");
 
         try(Connection connection = DriverManager.getConnection(dbUrl, username, password)){
             System.out.println("Connection created!");
         } catch (SQLException e) {
             System.out.println("Connection failed!");
+            System.out.println(e.getMessage());
         }
     }
 
